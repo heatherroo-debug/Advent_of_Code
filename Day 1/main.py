b2f84cfd8f7187e9526847a000eca9e9bcb1023d
heatherroo-debug <heather.r.mcleod@gmail.com>
2026-02-19 17:57:33 -0500

    Rotation instructions interpreted

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,3 +1,19 @@
-def main()
+import sys
+
+def get_input(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    input = get_input(sys.argv[1])
     zero_count = 0
+    current_position =0
+    rotate_list = input.split()
+    for instruction in rotate_list:
+        direction = instruction[0]
+        num = instruction[1:]
+        
     
+main()
