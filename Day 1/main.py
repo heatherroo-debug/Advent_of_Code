de911e8f245cd3ef0bbe069e56251bcd781f5fcd
heatherroo-debug <heather.r.mcleod@gmail.com>
2026-02-21 10:25:46 -0500

    Upload test cases and L/R instructions

--- Day 1/main.py
+++ Day 1/main.py
@@ -9,14 +9,32 @@ def get_input(path_to_file):
 def main():
     input = get_input(sys.argv[1])
     zero_count = 0
-    current_position = 0
+    current_position = 50
     rotate_list = input.split()
     for instruction in rotate_list:
         direction = instruction[0]
-        num = instruction[1:]
+        num = int(instruction[1:])
+        print(f"current position is {current_position}")
         if direction == "L":
-            current_position = current_position - num
+            result = current_position - num
+            if result < 0:
+                diff = num - current_position - 1
+                current_position = 99 - diff
+                print(f"rotation is {instruction}")
+                print(f"new position is {current_position}")
+            else:
+                current_position = result
+                print(f"new position is {current_position}")
+        if direction == "R":
+            result = current_position + num
+            if result > 99:
+                diff = num - current_position - 1
+                current_position = 0 + diff
+                print(f"rotation is {instruction}")
+                print(f"difference is {diff}")
+                print(f"new position is {current_position}")
         else:
-            current_position = current_position + num
-    
+            current_position = result
+
+
 main()
